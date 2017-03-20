a62852595b741f66bf597c0c550641eae127aad8
Shingo Hirayama <hirayamaru@gmail.com>
2017-03-20 11:30:31 +0900

    5.8 すべての記事を一覧表示する

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,10 @@ class ArticlesController < ApplicationController
   def new
   end
 
+  def index
+    @articles = Article.all
+  end
+
   private def article_params
     params.require(:article).permit(:title, :text)
   end
