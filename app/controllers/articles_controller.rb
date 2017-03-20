ef12fb513de3736ef270f3908f5f5ac8bded8e09
Shingo Hirayama <hirayamaru@gmail.com>
2017-03-20 11:51:16 +0900

    5.13 記事を削除する

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -40,6 +40,13 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+  
+    redirect_to articles_path
+  end
+
 end
 
 
