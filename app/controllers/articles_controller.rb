ef353ea75db5938c692361b662e80b66b77e0a57
Shingo Hirayama <hirayamaru@gmail.com>
2017-03-20 11:25:35 +0900

    5.6 コントローラでデータを保存する

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,15 @@ class ArticlesController < ApplicationController
   def new
   end
 
-  def create
-    render plain: params[:article].inspect
+  private def article_params
+    params.require(:article).permit(:title, :text)
   end
 
+  def create
+    @article = Article.new(article_params)
+  
+    @article.save
+    redirect_to @article
+  end
+  
 end
