f8076c656cf43d947324b48ace0bd6f5e959922f
Shingo Hirayama <hirayamaru@gmail.com>
2017-03-20 11:40:02 +0900

    5.10 検証 (バリデーション) の追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
 
   def new
+    @article = Article.new
   end
 
   def index
@@ -14,8 +15,11 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
   
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -23,3 +27,7 @@ class ArticlesController < ApplicationController
   end
 
 end
+
+
+ 
+ 
